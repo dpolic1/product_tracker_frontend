443e2311c1274fa0627771eb31e889a6f8d9fce6
dpolic <dpolic@comping.hr>
2024-11-02 18:28:32 +0100

    quick fix

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -6,7 +6,7 @@ import { keycloak } from "../keycloak/KeycloakConfiguration.jsx";
 
 export default function Header() {
   const isAuthenticated = keycloak.authenticated;
-  const isAdmin = keycloak.realmAccess?.roles?.includes("ADMIN_PRIVILEGE") || false;
+  const isAdmin = keycloak.realmAccess?.roles?.includes("ADMIN_PRIVILEGES") || false;
 
   const handleLogout = () => {
     keycloak.logout();
@@ -16,27 +16,27 @@ export default function Header() {
     return null;
   }
 
-  return (
-    <header className="main_header">
-      <nav className="header_nav">
-        <ul className="nav_list">
-          <li className="nav_list_item">
-            <Link to="/home" className="nav_link">
-              Home
-            </Link>
-          </li>
-          {isAdmin && (
+    return (
+      <header className="main_header">
+        <nav className="header_nav">
+          <ul className="nav_list">
             <li className="nav_list_item">
-              <Link to="/admin" className="nav_link">
-                Admin
+              <Link to="/home" className="nav_link">
+                Home
               </Link>
             </li>
-          )}
-        </ul>
-        <button className="logout_button" onClick={handleLogout}>
-          Logout
-        </button>
-      </nav>
-    </header>
-  );
-}
+            {isAdmin && (
+              <li className="nav_list_item">
+                <Link to="/admin" className="nav_link">
+                  Admin
+                </Link>
+              </li>
+            )}
+          </ul>
+          <button className="logout_button" onClick={handleLogout}>
+            Logout
+          </button>
+        </nav>
+      </header>
+    );
+  }

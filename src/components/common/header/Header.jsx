87b9fb6041a12a622e447f4ef536ab7531e3a0d2
dpolic <dpolic@comping.hr>
2024-11-02 18:37:25 +0100

    formatting

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -16,27 +16,27 @@ export default function Header() {
     return null;
   }
 
-    return (
-      <header className="main_header">
-        <nav className="header_nav">
-          <ul className="nav_list">
+  return (
+    <header className="main_header">
+      <nav className="header_nav">
+        <ul className="nav_list">
+          <li className="nav_list_item">
+            <Link to="/home" className="nav_link">
+              Home
+            </Link>
+          </li>
+          {isAdmin && (
             <li className="nav_list_item">
-              <Link to="/home" className="nav_link">
-                Home
+              <Link to="/admin" className="nav_link">
+                Admin
               </Link>
             </li>
-            {isAdmin && (
-              <li className="nav_list_item">
-                <Link to="/admin" className="nav_link">
-                  Admin
-                </Link>
-              </li>
-            )}
-          </ul>
-          <button className="logout_button" onClick={handleLogout}>
-            Logout
-          </button>
-        </nav>
-      </header>
-    );
-  }
+          )}
+        </ul>
+        <button className="logout_button" onClick={handleLogout}>
+          Logout
+        </button>
+      </nav>
+    </header>
+  );
+}

b890feac363fad2598dd9c1e2f1a22ebc0a1b412
dpolic <dpolic@comping.hr>
2024-12-13 20:27:59 +0100

    Updated new product form, removed codes from the form and added submit button

--- src/components/pages/new-product/components/NewProductForm.jsx
+++ src/components/pages/new-product/components/NewProductForm.jsx
@@ -4,22 +4,25 @@ import { keycloak } from "../../../common/keycloak/KeycloakConfiguration.jsx";
 export default function NewProductForm({
   handleFormSubmit,
 }) {
-  const [predictionData, setPredictionData] = useState(null); // Store API response
+  const [predictionData, setPredictionData] = useState(null); // Store API response for predictions
   const [loading, setLoading] = useState(false); // Manage loading state
-  const [gpcFamilies, setGpcFamilies] = useState([]); // Store the list of GPC families for dropdown
+  const [gpcFamilies, setGpcFamilies] = useState([]); // Store the list of GPC families
+  const [gpcClasses, setGpcClasses] = useState([]); // Store the list of GPC classes
+  const [gpcBricks, setGpcBricks] = useState([]); // Store the list of GPC bricks
+  const [formComplete, setFormComplete] = useState(false); // Check if the form is complete
 
   // Local state for form input fields
   const [formData, setFormData] = useState({
     gtin: "",
     name: "",
-    gpcSegmentCode: "",
     gpcSegmentText: "",
-    gpcFamilyCode: "",
     gpcFamilyText: "",
-    gpcClassCode: "",
     gpcClassText: "",
-    gpcBrickCode: "",
     gpcBrickText: "",
+    gpcSegmentCode: "",  // Store the code of the GPC segment
+    gpcFamilyCode: "",   // Store the code of the GPC family
+    gpcClassCode: "",    // Store the code of the GPC class
+    gpcBrickCode: "",    // Store the code of the GPC brick
   });
 
   // Handle the Predict button click and fetch data from the API
@@ -37,6 +40,7 @@ export default function NewProductForm({
 
       if (response.ok) {
         const data = await response.json();
+        console.log("Prediction data:", data);
         setPredictionData(data); // Update state with prediction data
       } else {
         console.error("Error fetching prediction data: ", response.statusText);
@@ -44,59 +48,156 @@ export default function NewProductForm({
     } catch (error) {
       console.error("Error fetching prediction data:", error);
     } finally {
-      setLoading(false); // Stop loading once the request is finished
+      setLoading(false);
     }
   };
 
   // Handle the prediction button click and update the input fields
-  const handlePredictionButtonClick = async (gpcBrickCode, gpcBrickText) => {
-    // Update the form fields with the clicked prediction data
+  const handlePredictionButtonClick = async (prediction) => {
     setFormData({
       ...formData,
-      gpcSegmentCode: gpcBrickCode,
-      gpcSegmentText: gpcBrickText,
+      gpcSegmentText: prediction.gpcSegmentText,
+      gpcSegmentCode: prediction.gpcSegmentCode,
+      gpcFamilyText: "",
+      gpcFamilyCode: "",
+      gpcClassText: "",
+      gpcClassCode: "",
+      gpcBrickText: "",
+      gpcBrickCode: "", // Clear all related fields
     });
 
-    // Fetch GPC families for the selected gpcSegmentCode
+    // Call API to fetch GPC families based on the segment code
+    await fetchGpcFamilies(prediction.gpcSegmentCode);
+  };
+
+  // Fetch GPC families based on the gpcSegmentCode
+  const fetchGpcFamilies = async (segmentCode) => {
     try {
-      const response = await fetch(`http://localhost:8101/gpc-families/gpc-segment/${gpcBrickCode}`, {
+      setLoading(true);
+      const response = await fetch(`http://localhost:8101/gpc-families/gpc-segment/${segmentCode}`, {
         method: "GET",
         headers: {
-          Authorization: `Bearer ${keycloak.token}`, // Keycloak token for authorization
+          Authorization: `Bearer ${keycloak.token}`,
           "Content-Type": "application/json",
         },
       });
 
       if (response.ok) {
         const data = await response.json();
-        setGpcFamilies(data); // Populate the GPC family options
+        setGpcFamilies(data); // Update GPC families state
       } else {
-        console.error("Error fetching GPC families:", response.statusText);
+        console.error("Error fetching GPC families: ", response.statusText);
       }
     } catch (error) {
       console.error("Error fetching GPC families:", error);
+    } finally {
+      setLoading(false);
     }
-
-    // Remove the prediction buttons by setting predictionData to null
-    setPredictionData(null);
   };
 
-  // Handle form input change (to update local state)
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+  // Handle family selection and fetch classes
+  const handleFamilyChange = async (e) => {
+    const selectedFamilyText = e.target.value;
+    const selectedFamily = gpcFamilies.find(family => family.text === selectedFamilyText);
+
+    setFormData({
+      ...formData,
+      gpcFamilyText: selectedFamilyText,
+      gpcFamilyCode: selectedFamily.code,  // Store the code
+    });
+
+    // Fetch GPC classes based on the selected family's code
+    await fetchGpcClasses(selectedFamily.code);
   };
+
+  // Fetch GPC classes based on the selected family's code
+  const fetchGpcClasses = async (familyCode) => {
+    try {
+      setLoading(true);
+      const response = await fetch(`http://localhost:8101/gpc-classes/gpc-family/${familyCode}`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${keycloak.token}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setGpcClasses(data); // Update GPC classes state
+      } else {
+        console.error("Error fetching GPC classes: ", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching GPC classes:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Handle class selection and fetch bricks
+  const handleClassChange = async (e) => {
+    const selectedClassText = e.target.value;
+    const selectedClass = gpcClasses.find(gpcClass => gpcClass.text === selectedClassText);
+
+    setFormData({
+      ...formData,
+      gpcClassText: selectedClassText,
+      gpcClassCode: selectedClass.code,  // Store the code
+    });
+
+    // Fetch GPC bricks based on the selected class's code
+    await fetchGpcBricks(selectedClass.code);
+  };
+
+  // Fetch GPC bricks based on the selected class's code
+  const fetchGpcBricks = async (classCode) => {
+    try {
+      setLoading(true);
+      const response = await fetch(`http://localhost:8101/gpc-bricks/gpc-class/${classCode}`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${keycloak.token}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setGpcBricks(data); // Update GPC bricks state
+      } else {
+        console.error("Error fetching GPC bricks: ", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching GPC bricks:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Handle brick selection and update the field
+  const handleBrickChange = (e) => {
+    const selectedBrickText = e.target.value;
+    const selectedBrick = gpcBricks.find(brick => brick.text === selectedBrickText);
+
+    setFormData({
+      ...formData,
+      gpcBrickText: selectedBrickText,
+      gpcBrickCode: selectedBrick.code,  // Store the code
+    });
+    setFormComplete(true); // Mark the form as complete
+  };
+
+  // Check if the form is valid for submission
+  const isFormValid = formData.gpcBrickText !== ""; // Ensure brick is selected
 
   return (
     <div className="relative overflow-x-auto bg-gray-50 dark:bg-gray-800 p-6 rounded-lg shadow-lg w-full">
       <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-100 mb-4">New Product</h3>
       <form onSubmit={handleFormSubmit}>
-        <div className="grid grid-cols-[65%_33%] gap-4">
-          {/* Left Side of the Form */}
-          <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-[70%_30%] gap-4">
+          {/* Left Side of the Form (Single Column Layout) */}
+          <div className="space-y-4">
             {/* GTIN */}
             <div className="mb-4">
               <label htmlFor="gtin" className="block text-sm font-medium text-gray-700 dark:text-gray-300">
@@ -107,7 +208,7 @@ export default function NewProductForm({
                 id="gtin"
                 name="gtin"
                 value={formData.gtin}
-                onChange={handleInputChange}
+                onChange={(e) => setFormData({ ...formData, gtin: e.target.value })}
                 className="mt-1 block w-full px-3 py-2 text-gray-700 dark:text-gray-300 bg-gray-100 dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md"
                 required
               />
@@ -123,98 +224,130 @@ export default function NewProductForm({
                 id="name"
                 name="name"
                 value={formData.name}
-                onChange={handleInputChange}
+                onChange={(e) => setFormData({ ...formData, name: e.target.value })}
                 className="mt-1 block w-full px-3 py-2 text-gray-700 dark:text-gray-300 bg-gray-100 dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md"
                 required
               />
             </div>
 
             {/* GPC Fields */}
-            {[
-              { label: "GPC Segment Code", id: "gpcSegmentCode" },
-              { label: "GPC Segment Text", id: "gpcSegmentText" },
-              { label: "GPC Family Code", id: "gpcFamilyCode" },
-              { label: "GPC Family Text", id: "gpcFamilyText" },
-              { label: "GPC Class Code", id: "gpcClassCode" },
-              { label: "GPC Class Text", id: "gpcClassText" },
-              { label: "GPC Brick Code", id: "gpcBrickCode" },
-              { label: "GPC Brick Text", id: "gpcBrickText" },
-            ].map((field, index) => (
-              <div className="mb-4" key={index}>
-                <label htmlFor={field.id} className="block text-sm font-medium text-gray-700 dark:text-gray-300">
-                  {field.label}
-                </label>
-                {field.id === "gpcFamilyText" ? (
-                  // Render GPC Family Text as a dropdown if GPC families are available
-                  <select
-                    id={field.id}
-                    name={field.id}
-                    value={formData.gpcFamilyText}
-                    onChange={handleInputChange}
-                    className="mt-1 block w-full px-3 py-2 text-gray-700 dark:text-gray-300 bg-gray-100 dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md"
-                    required
-                  >
-                    <option value="">Select a GPC Family</option>
-                    {gpcFamilies.map((family, index) => (
-                      <option key={index} value={family.text}>
-                        {family.text}
-                      </option>
-                    ))}
-                  </select>
-                ) : (
-                  // Render other fields as text inputs
-                  <input
-                    type="text"
-                    id={field.id}
-                    name={field.id}
-                    value={formData[field.id]}
-                    onChange={handleInputChange}
-                    className="mt-1 block w-full px-3 py-2 text-gray-700 dark:text-gray-300 bg-gray-100 dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md"
-                    required
-                  />
-                )}
-              </div>
-            ))}
-          </div>
+            <div className="mb-4">
+              <label htmlFor="gpcSegmentText" className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+                GPC Segment
+              </label>
+              <input
+                type="text"
+                id="gpcSegmentText"
+                name="gpcSegmentText"
+                value={formData.gpcSegmentText}
+                onChange={(e) => setFormData({ ...formData, gpcSegmentText: e.target.value })}
+                className="mt-1 block w-full px-3 py-2 text-gray-700 dark:text-gray-300 bg-gray-100 dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md"
+                required
+              />
+            </div>
 
-          {/* Right Side of the Form (Third Column) */}
-          <div className="mb-4 flex flex-col">
-            {/* Predict Button */}
-            <button
-              type="button"
-              onClick={handlePredictClick}
-              className="bg-blue-600 text-white px-4 py-2 mt-6 rounded-md hover:bg-blue-700"
-              disabled={loading} // Disable the button while loading
-            >
-              {loading ? "Loading..." : "Predict"}
-            </button>
-
-            {/* Prediction Results as Buttons */}
-            {predictionData && (
-              <div className="mt-4 space-y-2">
-                {predictionData.map((prediction, index) => (
-                  <button
-                    key={index}
-                    type="button"
-                    className="bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700"
-                    onClick={() => handlePredictionButtonClick(prediction.gpcBrickCode, prediction.gpcBrickText)}
-                  >
-                    {`Prediction ${index + 1}: ${prediction.gpcBrickCode} - ${prediction.gpcBrickText} (Confidence: ${prediction.predictionConfidence.toFixed(2)})`}
-                  </button>
+            {/* GPC Family */}
+            <div className="mb-4">
+              <label htmlFor="gpcFamilyText" className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+                GPC Family
+              </label>
+              <select
+                id="gpcFamilyText"
+                name="gpcFamilyText"
+                value={formData.gpcFamilyText}
+                onChange={handleFamilyChange}
+                className="mt-1 block w-full px-3 py-2 text-gray-700 dark:text-gray-300 bg-gray-100 dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md"
+                required
+              >
+                <option value="">Select a GPC Family</option>
+                {gpcFamilies.map((family, index) => (
+                  <option key={index} value={family.text}>
+                    {family.text}
+                  </option>
                 ))}
-              </div>
-            )}
-          </div>
-        </div>
+              </select>
+            </div>
 
-        {/* Submit Button */}
-        <div className="flex justify-end mt-4">
+            {/* GPC Class */}
+            <div className="mb-4">
+              <label htmlFor="gpcClassText" className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+                GPC Class
+              </label>
+              <select
+                id="gpcClassText"
+                name="gpcClassText"
+                value={formData.gpcClassText}
+                onChange={handleClassChange}
+                className="mt-1 block w-full px-3 py-2 text-gray-700 dark:text-gray-300 bg-gray-100 dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md"
+                required
+              >
+                <option value="">Select a GPC Class</option>
+                {gpcClasses.map((gpcClass, index) => (
+                  <option key={index} value={gpcClass.text}>
+                    {gpcClass.text}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            {/* GPC Brick */}
+            <div className="mb-4">
+              <label htmlFor="gpcBrickText" className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+                GPC Brick
+              </label>
+              <select
+                id="gpcBrickText"
+                name="gpcBrickText"
+                value={formData.gpcBrickText}
+                onChange={handleBrickChange}
+                className="mt-1 block w-full px-3 py-2 text-gray-700 dark:text-gray-300 bg-gray-100 dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md"
+                required
+              >
+                <option value="">Select a GPC Brick</option>
+                {gpcBricks.map((brick, index) => (
+                  <option key={index} value={brick.text}>
+                    {brick.text}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+
+           {/* Right Side: Prediction Buttons and Submit Button */}
+        <div className="flex flex-col h-full justify-between space-y-4">
+          {/* Predict Categories Button */}
           <button
-            type="submit"
-            className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700"
+            type="button"
+            onClick={handlePredictClick}
+            className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 mt-6"
           >
-            Create Product
+            {loading ? "Loading..." : "Predict Categories"}
           </button>
+
+            {predictionData && (
+            <div className="mt-4 space-y-2">
+              {predictionData.map((prediction, index) => (
+                <button
+                  key={index}
+                  type="button"
+                  className="bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700"
+                  onClick={() => handlePredictionButtonClick(prediction)}
+                >
+                  {`Prediction ${index + 1}: ${prediction.gpcSegmentText} (Confidence: ${prediction.predictionConfidence.toFixed(2)})`}
+                </button>
+              ))}
+            </div>
+          )}
+
+            {/* Submit Button at the Bottom */}
+            <button
+              type="submit"
+              disabled={!formComplete}
+              className={`px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 mt-6 ${!formComplete ? 'cursor-not-allowed opacity-50' : ''}`}
+            >
+              Submit
+            </button>
+          </div>
         </div>
       </form>
     </div>
